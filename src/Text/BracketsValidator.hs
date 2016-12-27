d9ff9444dbd931e22afdb7f822993df2f390adba
Ignat Insarov <kindaro@gmail.com>
2016-12-28 00:13:37 +0300

    Refactor: Put types in their own module.

--- src/Text/BracketsValidator.hs
+++ src/Text/BracketsValidator.hs
@@ -1,28 +1,11 @@
-module Text.BracketsValidator where
+module Text.BracketsValidator
+    ( lexer
+    , parser
+    , report
+    , module Text.BracketsValidator.Types
+    ) where
 
-data Symbol = ORound | OSquare | OCurled | CRound | CSquare | CCurled | Blank String
-    deriving (Eq, Show, Read)
-
-isOpen x = case x of
-    ORound -> True
-    OSquare -> True
-    OCurled -> True
-    _ -> False
-
-isClose x = case x of
-    CRound -> True
-    CSquare -> True
-    CCurled -> True
-    _ -> False
-
-isBlank x = case x of
-    Blank _ -> True
-    _ -> False
-
-isMatching ORound CRound = True
-isMatching OSquare CSquare = True
-isMatching OCurled CCurled = True
-isMatching _ _ = False
+import Text.BracketsValidator.Types
 
 lexer :: String -> [Symbol]
 lexer [] = []
@@ -40,43 +23,17 @@ lexer (x:xs)
 
 insert :: Symbol -> [Symbol] -> Validation [Symbol]
 insert s a = (flip add 1) $ insert' s a
+    where
+    add (Validation s x) n = Validation ( s { position = position s + n }) x
+
 insert' (Blank _) a = pure a
 insert' s a
     | isOpen s = pure (s:a)
     | (not . null) a && (head a) `isMatching` s = pure (tail a)
     | otherwise = impure (s:a)
-
-data State = State { position :: Integer, status :: Bool }
-    deriving (Eq, Read, Show)
-
-data Validation x = Validation State x
-    deriving (Eq, Read, Show)
-
-instance Monoid State where
-    mempty = State { position = 0, status = True }
-    x `mappend` y
-        | status x && status y = State { position = position x + position y, status = True }
-        | otherwise = State { position = position x + position y, status = False }
-
-instance Functor Validation where
-    fmap f (Validation s a) = Validation s (f a)
-
-instance Applicative Validation where
-    pure x = Validation mempty x
-    (Validation s f) <*> (Validation t x) = Validation (s `mappend` t) (f x)
-
-add (Validation s x) n = Validation ( s { position = position s + n }) x
-taint (Validation s a) = Validation (s { status = False }) a
-impure x = taint $ pure x
-
-instance Monad Validation where
-    a@(Validation s x) >>= f
-        | status s == False = Validation s (unf x) -- Do nothing.
-        | otherwise = s `into` f x
-        -- Maybe I don't need this part.
-        where unpack (Validation s a) = a
-              unf = unpack . f
-              into s (Validation t y) = Validation (s `mappend` t) y
+    where
+    taint (Validation s a) = Validation (s { status = False }) a
+    impure x = taint $ pure x
 
 parser :: [Symbol] -> Validation [Symbol]
 parser = ( foldl (>>=) (return []) ) . (fmap insert)
@@ -90,3 +47,4 @@ report (Validation state stack)
             ++ (show $ length stack) ++ " symbols left in stack."
     | otherwise = "Validation failed after " ++ (show $ position state) ++ " symbols parsed. "
             ++ (show $ length stack) ++ " symbols left in stack."
+
---
+++ src/Text/BracketsValidator/Types.hs
@@ -0,0 +1,61 @@
+module Text.BracketsValidator.Types
+    ( Symbol (..)
+    , State (..)
+    , Validation (..)
+    , isOpen
+    , isClose
+    , isBlank
+    , isMatching
+    ) where
+
+data Symbol = ORound | OSquare | OCurled | CRound | CSquare | CCurled | Blank String
+    deriving (Eq, Show, Read)
+
+isOpen x = case x of
+    ORound -> True
+    OSquare -> True
+    OCurled -> True
+    _ -> False
+
+isClose x = case x of
+    CRound -> True
+    CSquare -> True
+    CCurled -> True
+    _ -> False
+
+isBlank x = case x of
+    Blank _ -> True
+    _ -> False
+
+isMatching ORound CRound = True
+isMatching OSquare CSquare = True
+isMatching OCurled CCurled = True
+isMatching _ _ = False
+
+data State = State { position :: Integer, status :: Bool }
+    deriving (Eq, Read, Show)
+
+instance Monoid State where
+    mempty = State { position = 0, status = True }
+    x `mappend` y
+        | status x && status y = State { position = position x + position y, status = True }
+        | otherwise = State { position = position x + position y, status = False }
+
+data Validation x = Validation State x
+    deriving (Eq, Read, Show)
+
+instance Functor Validation where
+    fmap f (Validation s a) = Validation s (f a)
+
+instance Applicative Validation where
+    pure x = Validation mempty x
+    (Validation s f) <*> (Validation t x) = Validation (s `mappend` t) (f x)
+
+instance Monad Validation where
+    a@(Validation s x) >>= f
+        | status s == False = Validation s (unf x) -- Do nothing.
+        | otherwise = s `into` f x
+        -- Maybe I don't need this part.
+        where unpack (Validation s a) = a
+              unf = unpack . f
+              into s (Validation t y) = Validation (s `mappend` t) y
